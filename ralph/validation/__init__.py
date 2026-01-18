882347d48c7bb21ef67d4ef7c06a529ea2b72de2
João Pereira <81766337+sh0ck-zy@users.noreply.github.com>
2026-01-18 02:52:00 +0100

    feat: US-003 - Standardize 'action' extraction from analysis_reports for backtesting

---
+++ ralph/validation/__init__.py
@@ -0,0 +1,29 @@
+from validation.action_extractor import Action, ActionType, BetSelection, extract_action
+from validation.engine import ValidationEngine, ValidationRecord
+from validation.report_schema import (
+    BaselineMetrics,
+    BettingMetrics,
+    CalibrationStats,
+    NarrativeMetrics,
+    OutcomeMetrics,
+    PromptVersionReport,
+    ValidationReport,
+    report_schema,
+)
+
+__all__ = [
+    "Action",
+    "ActionType",
+    "BetSelection",
+    "BaselineMetrics",
+    "BettingMetrics",
+    "CalibrationStats",
+    "NarrativeMetrics",
+    "OutcomeMetrics",
+    "PromptVersionReport",
+    "ValidationEngine",
+    "ValidationRecord",
+    "ValidationReport",
+    "extract_action",
+    "report_schema",
+]
